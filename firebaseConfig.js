84c481486c6fd4c7e3497065c9c2e73f30d20eb9
00Fenris <ignaciomopa@hotmail.com>
2025-08-21 13:31:53 +0200

    Subir firebaseConfig.js temporalmente para la demo

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+// Configuración de Firebase
+// Configuración de Firebase (solo objeto, sin imports ni inicialización)
+const firebaseConfig = {
+  apiKey: "AIzaSyA59Jl1NsshafjLVzuQswpc3-4BsGUgIBI",
+  authDomain: "proyecto-psicologos-7f53f.firebaseapp.com",
+  projectId: "proyecto-psicologos-7f53f",
+  storageBucket: "proyecto-psicologos-7f53f.appspot.com",
+  messagingSenderId: "1032754415458",
+  appId: "1:1032754415458:web:e0975b2549ec1373584e3b",
+  measurementId: "G-DBBG2CJFQ7"
+};
